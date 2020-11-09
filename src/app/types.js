febcef8315b0d490640c687b4758f7dbd5fb217c
o.kuzmenko <eight.alex@gmail.com>
2020-11-09 18:11:44 +0200

    Fix time in title

--- src/app/types.js
+++ src/app/types.js
@@ -5,3 +5,11 @@
 /**
  * @typedef {'uk_UA' | 'ru_RU' | 'en_US'} LocaleKey
  */
+
+/**
+ * @typedef {object} CustomTime
+ * @property {string} days
+ * @property {string} hours
+ * @property {string} minutes
+ * @property {string} seconds
+ */
